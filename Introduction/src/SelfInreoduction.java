dbb92a64d12239bd3834cbcc51f7a996dd67958d
Chandu150693 <pallichandrasekhar93@gmail.com>
2026-02-14 23:26:03 +0530

    Code Updated to navigate to google

--- Introduction/src/SelfInreoduction.java
+++ Introduction/src/SelfInreoduction.java
@@ -1,10 +1,18 @@
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SelfInreoduction {
 	public static void main(String[] args) {
-		
-		ChromeDriver driver = new ChromeDriver();
-		
-		
+		// Invoking Browser
+		// Chrome - ChromeDriver
+		// Firefox - GeckoDriver
+		// Edge - EdgeDriver
+		// Safari - SafariDriver
+		// WebDriver - Interface
+		WebDriver driver = new ChromeDriver();
+//		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println("Title of the page is: "+ driver.getTitle());
+		driver.close();
 	}
 }
