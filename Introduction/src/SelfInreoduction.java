f5dcc3481bf3452c84763d9a4a470e629b460690
Chandu150693 <pallichandrasekhar93@gmail.com>
2026-02-15 22:20:18 +0530

    Edge Driver

--- Introduction/src/SelfInreoduction.java
+++ Introduction/src/SelfInreoduction.java
@@ -1,20 +1,21 @@
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class SelfInreoduction {
 	public static void main(String[] args) {
 		// Invoking Browser
-		// Chrome - ChromeDriver
-		// Firefox - GeckoDriver
-		// Edge - EdgeDriver
 		// Safari - SafariDriver
 		// WebDriver - Interface
 
+		// Chrome Driver -> chromedriver.exe
 //		WebDriver driver = new ChromeDriver();
 //		ChromeDriver driver = new ChromeDriver();
 
 		
-		// FireFox Driver
-		FirefoxDriver driver = new FirefoxDriver();
+		// FireFox Driver -> geckodriver.exe
+		//FirefoxDriver driver = new FirefoxDriver();
+		
+		// Edge Driver -> msedgedriver.exe
+		EdgeDriver driver = new EdgeDriver();
 		driver.get("https://www.google.com/");
 		System.out.println("Title of the page is: "+ driver.getTitle());
 		System.out.println("Current URl is: "+ driver.getCurrentUrl());
