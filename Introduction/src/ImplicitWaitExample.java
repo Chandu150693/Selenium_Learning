4d8e8d4cb2f46e65c7c344d833403cf30443ce2b
Chandu150693 <pallichandrasekhar93@gmail.com>
2026-02-18 07:28:01 +0530

    Added assertions

--- Introduction/src/ImplicitWaitExample.java
+++ Introduction/src/ImplicitWaitExample.java
@@ -3,16 +3,19 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+
 
 public class ImplicitWaitExample {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		// Create a new instance of the Edge driver
 		WebDriver driver = new EdgeDriver();
 
 		// Set an implicit wait time of 10 seconds
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+		driver.manage().window().maximize();
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		// ID
 		driver.findElement(By.id("inputUsername")).sendKeys("Chandu");	
@@ -50,13 +53,34 @@ public class ImplicitWaitExample {
 		
 		driver.findElement(By.xpath("//button[text()='Go to Login']")).click();
 		
+		Thread.sleep(2000); // Explicit wait actually, need to add but thread sleep is used.
 		// ID
 		driver.findElement(By.id("inputUsername")).sendKeys("Chandu");	
 		// Name
 		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		
+		driver.findElement(By.id("chkboxOne")).click();
+		
+		driver.findElement(By.xpath("//input[@name='chkboxTwo']")).click();
+		
 		driver.findElement(By.className("signInBtn")).click();
 		// Close the browser
-//		driver.close();
+		
+		Thread.sleep(3000);
+		
+		String helloTxt = driver.findElement(By.xpath("//div[@class='login-container']/h2")).getText();
+		String welcomeTxt = driver.findElement(By.xpath("//div[@class='login-container']/h1")).getText();
+		String sucessTxt = driver.findElement(By.xpath("//div[@class='login-container']/p")).getText();
+		
+		System.out.println(helloTxt);
+		System.out.println(welcomeTxt);
+		
+		Assert.assertEquals(sucessTxt, "You are successfully logged in.");
+		
+		driver.findElement(By.cssSelector(".logout-btn")).click();
+				
+		driver.close();
+		System.out.println(sucessTxt+" \nTest case passed successfully.");
+		
 	}
 }
